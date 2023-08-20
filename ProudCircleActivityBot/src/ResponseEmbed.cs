67b68bb52359b99abc734a00708060f09d7279f2
illyum <90023277+itzilly@users.noreply.github.com>
2023-08-20 17:19:14 -0600

    Add default embed helper class

---
+++ ProudCircleActivityBot/src/ResponseEmbed.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus.Entities;
+
+namespace ProudCircleActivityBot; 
+
+public class ResponseEmbed {
+    public DiscordEmbedBuilder EmbedBuilder { get; set; }
+    private readonly VersionInfo _versionInfo = new VersionInfo();
+    
+    public ResponseEmbed() {
+        EmbedBuilder = new DiscordEmbedBuilder();
+        // EmbedBuilder.WithAuthor("illyum");
+        EmbedBuilder.WithColor(new DiscordColor("#0ec7c7"));
+        EmbedBuilder.WithTimestamp(DateTimeOffset.Now);
+        EmbedBuilder.WithFooter($"Activity Tracker | {_versionInfo.PrettyName}");
+    }
+}
