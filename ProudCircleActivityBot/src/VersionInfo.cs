0e9bea1ec8b99c94692767da02dc5e3f06f6b20e
illyum <90023277+itzilly@users.noreply.github.com>
2023-08-20 17:38:55 -0600

    Update Version -> 0.0.1

--- ProudCircleActivityBot/src/VersionInfo.cs
+++ ProudCircleActivityBot/src/VersionInfo.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProudCircleActivityBot; 
 
 public class VersionInfo {
-    public string PrettyName = "Dev - v0.0.1";
-    public string Name = "0.0.1";
+    public string PrettyName = "Dev - v0.0.2";
+    public string Name = "0.0.2";
 }
