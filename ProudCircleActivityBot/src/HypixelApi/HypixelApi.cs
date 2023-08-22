b9f40c9e4a5458e93b5459ba7a22d7c83c1b6237
illyum <90023277+itzilly@users.noreply.github.com>
2023-08-22 05:23:52 -0600

    Add simple api

---
+++ ProudCircleActivityBot/src/HypixelApi/HypixelApi.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using ProudCircleActivityBot.HypixelApi.Replies;
+
+namespace ProudCircleActivityBot.HypixelApi;
+
+public class HypixelApi {
+    public string BaseUrl = "https://api.hypixel.net/";
+    public HypixelApiKey Key { get; private set; }
+    private HttpClient HttpClient;
+
+    public HypixelApi(HypixelApiKey key) {
+        Key = key;
+    }
+
+    public async Task<PlayerReply> GetPlayerByUuidAsync(Guid guid) {
+        try {
+            string apiUrl = $"{BaseUrl}player?uuid={guid}";
+            HttpClient.DefaultRequestHeaders.Add("API-Key", Key.ToString());
+            HttpResponseMessage response = await HttpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode) {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                PlayerReply playerData = JsonConvert.DeserializeObject<PlayerReply>(jsonResponse);
+                return playerData;
+            }
+            else {
+                throw new HttpRequestException($"HTTP request failed with status code {response.StatusCode}");
+            }
+        }
+        catch (Exception ex) {
+            throw ex;
+        }
+    }
+}
---
+++ ProudCircleActivityBot/src/HypixelApi/HypixelApiKey.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ProudCircleActivityBot.HypixelApi; 
+
+public struct HypixelApiKey {
+    public string ApiKey;
+    private readonly Regex KeyPattern = new Regex(@"^[0-9a-fA-F]{32}$");
+    
+    public HypixelApiKey(string Key) {
+        ApiKey = Key;
+    }
+    
+    public HypixelApiKey(string key, bool TestKeyPattern = false) : this() {
+        if (!TestKeyPattern) {
+            ApiKey = key;
+            return;
+        }
+
+        if (MatchesKeyPattern(key)) {
+            ApiKey = key;
+            return;
+        }
+
+        throw new ArgumentException("Invalid Key Pattern", paramName: key);
+    }
+
+    public bool MatchesKeyPattern(string key) {
+        if (string.IsNullOrEmpty(key)) {
+            throw new ArgumentNullException(paramName: key);
+        }
+
+        return KeyPattern.IsMatch(key);
+    }
+}
