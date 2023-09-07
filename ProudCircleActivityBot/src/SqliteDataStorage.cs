85991c042b6670d9aaca6cc82c0fb6f5523025ae
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-07 14:55:09 -0600

    Create SqliteDataStorage.cs

---
+++ ProudCircleActivityBot/src/SqliteDataStorage.cs
@@ -0,0 +1,15 @@
+﻿namespace ProudCircleActivityBot; 
+
+public class SqliteDataStorage : IDataStorage {
+    public void Initialize() {
+        
+    }
+
+    public void InsertPlayer() {
+        
+    }
+
+    public void CleanUp() {
+        
+    }
+}
