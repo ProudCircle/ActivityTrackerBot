fd4cdc1f114cd9a796f385d870acfe8b658fd97a
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-07 14:55:23 -0600

    Create YamlConfLoader.cs

---
+++ ProudCircleActivityBot/src/YamlConfLoader.cs
@@ -0,0 +1,15 @@
+﻿using YamlDotNet.Serialization;
+
+namespace ProudCircleActivityBot; 
+
+public class YamlConfLoader {
+    private string _configPath = "settings.yaml";
+
+    public YamlConf YamlConf { get; private set; }
+
+    public void LoadConfig() {
+        string confContent = File.ReadAllText(_configPath);
+        var deserializer = new DeserializerBuilder().Build();
+        YamlConf = deserializer.Deserialize<YamlConf>(confContent);
+    }
+}
